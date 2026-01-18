6bec1a9d1cc56be436e5b33e57305c2787715f51
FrancescFe <fraferru@gmail.com>
2026-01-18 19:56:59 +0100

    Feat (008): update main to wire application

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,25 @@
 package org.francescfe
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    // TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, $name!")
+import org.francescfe.mowerrobot.application.port.input.ExecuteRobotsUseCase
+import org.francescfe.mowerrobot.application.usecase.ExecuteRobotsInteractor
+import org.francescfe.mowerrobot.infrastructure.input.cli.InputParser
+import org.francescfe.mowerrobot.infrastructure.output.cli.ConsoleResultPrinter
 
-    for (i in 1..5) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+fun main() {
+    try {
+        val rawInput = generateSequence(::readLine).toList()
+
+        val inputParser = InputParser()
+        val resultPrinter = ConsoleResultPrinter()
+
+        val useCase: ExecuteRobotsUseCase = ExecuteRobotsInteractor()
+
+        val command = inputParser.parse(rawInput)
+
+        val results = useCase.execute(command)
+
+        resultPrinter.print(results)
+    } catch (ex: Exception) {
+        System.err.println("ERROR: ${ex.message}")
     }
 }
